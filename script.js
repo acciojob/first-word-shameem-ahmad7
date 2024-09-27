1772be3fad2160f993330c156bacc06c960c6d6a
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-27 08:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
+	// Trim any leading or trailing spaces and split the string by spaces
+  const trimmedStr = str.trim();
+  
+  // Find the index of the first space
+  const firstSpaceIndex = trimmedStr.indexOf(' ');
+  
+  // If there's no space, return the entire string; otherwise, return the substring before the first space
+  return firstSpaceIndex === -1 ? trimmedStr : trimmedStr.slice(0, firstSpaceIndex);
 }
 
 // Do not change the code below
